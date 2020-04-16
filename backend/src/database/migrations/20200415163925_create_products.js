f1dc2e37b5c232ff667bcacbabeb05f7a42c1220
adrianora <adriano.araujo@ccc.ufcg.edu.br>
2020-04-16 01:11:51 -0300

    estrutura frontend + component login

--- backend/src/database/migrations/20200415163925_create_products.js
+++ backend/src/database/migrations/20200415163925_create_products.js
@@ -4,9 +4,9 @@ exports.up = function(knex) {
         table.increments();
         table.string('name').notNullable();
         table.string('tag').notNullable();
-        table.int('countert').notNullable();
-        table.decimal('value').notNullable();
-        table.string('photo').notNullable(); /* a pesquisar tipo*/
+        table.int('amount').notNullable();
+        table.decimal('price').notNullable();
+        table.string('image').notNullable(); /* a pesquisar tipo*/
         
         /*lembrar de dar o "npx knex migrate:latest" quando adcionar os elemenos a entidade*/
     })
---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import GoogleLogin from "react-google-login";
+
+import './styles.css';
+
+export default function Login() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [url, setUrl] = useState("");
+
+    const responseGoogle = response => {
+        setName(response.profileObj.name);
+        setEmail(response.profileObj.email);
+        setUrl(response.profileObj.imageUrl);
+    };
+
+    return (
+        <div className="App">
+            <h1>Login with Google</h1>
+            <h2>Welcome: {name}</h2>
+            <h2>Email: {email}</h2>
+            <img src={url} alt={name} />
+            <GoogleLogin
+                clientId="327559392172-5pbfegfr9087bo8cl8n97f1bqk9d161n.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={"single_host_origin"}
+            />
+        </div>
+    );
+}
