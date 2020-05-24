0805cb137b74a9bf6b62d705371ba90882226912
LuizBoas <luiz.boas@ccc.ufcg.edu.br>
2020-05-24 20:29:14 -0300

    select, register styles

--- mobile/src/pages/Register/index.js
+++ mobile/src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text, Image, View, TextInput, TouchableOpacity, CheckBox } from 'react-native';
+import { Text, Image, View, TextInput, TouchableOpacity, CheckBox, Picker } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 
 import api from '../../service/api.js';
@@ -11,12 +11,13 @@ import styles from './styles';
 
 export default function Register(){
     const [name, setName] = useState('');
-    const [tag, setTag] = useState('');
+    const [tag, setTag] = useState('TVs');
     const [amount, setAmount] = useState('');
-    const [price, setPrice] = useState('');
+    const [price, setPrice] = useState(''); 
     const [image, setImage] = useState('');
 
     const user = "user";
+    
 
     function navigateBack(){
         navigation.goBack()
@@ -26,7 +27,7 @@ export default function Register(){
 
     function resetForm() {
         setName("");
-        setTag("");
+        setTag("TVs");
         setAmount("");
         setPrice("");
         setImage("");
@@ -63,49 +64,57 @@ export default function Register(){
                     <Image source={back}/>
                 </TouchableOpacity>
             </View>
+     
+            <View style={styles.product}>
+                <Text style={styles.productsPropertyName}>Nome:</Text>
+                <TextInput
+                    style={styles.productsValue}
+                    placeholder="Digite o nome do produto"
+                    onChangeText={(text) => setName(text)}
+                    value={name}
+                /> 
 
-            <Text style={styles.productsPropertyName}>Nome:</Text>
-            <TextInput
-                style={{ height: 40, borderWidth: 1 }}
-                placeholder="Digite o nome do produto"
-                onChangeText={(text) => setName(text)}
-                value={name}
-            />
+                <Text style={styles.productsProperty}>Categoria:</Text>
+                <Picker
+                     selectedValue={tag}
+                     style={styles.select}
+                     onValueChange={(itemValue) => setTag(itemValue)}
+                >
+                    <Picker.Item label="TVs" value="TVs" />
+                    <Picker.Item label="Eletrodomésticos" value="Eletrodomésticos" />
+                    <Picker.Item label="Celulares" value="Celulares" />
+                    <Picker.Item label="Videogames" value="Videogames" />
+                     
+                </Picker>
+        
+                <Text style={styles.productsProperty}>Quantidade:</Text>
+                <TextInput
+                    style={styles.productsValue}
+                    
+                    placeholder="Digite a quantidade"
+                    onChangeText={(text) => setAmount(text)}
+                    value={amount}
+                /> 
 
-            <Text style={styles.productsProperty}>Tag:</Text>
-            <TextInput
-                style={{ height: 40, borderWidth: 1 }}
-                placeholder="Digite a categoria do produto"
-                onChangeText={(text) => setTag(text)}
-                value={tag}
-            />
+                <Text style={styles.productsProperty}>Preço:</Text>
+                <TextInput
+                    style={styles.productsValue}
+                    placeholder="Digite o preço do produto"
+                    onChangeText={(text) => setPrice(text)}
+                    value={price}
+                /> 
 
-            <Text style={styles.productsProperty}>Quantidade:</Text>
-            <TextInput
-                style={{ height: 40, borderWidth: 1 }}
-                placeholder="Digite a quantidade"
-                onChangeText={(text) => setAmount(text)}
-                value={amount}
-            />
-
-            <Text style={styles.productsProperty}>Preço:</Text>
-            <TextInput
-                style={{ height: 40, borderWidth: 1 }}
-                placeholder="Digite o preço do produto"
-                onChangeText={(text) => setPrice(text)}
-                value={price}
-            />
-
-            <Text style={styles.productsProperty}>Imagem:</Text>
-            <TextInput
-                style={{ height: 40, borderWidth: 1 }}
-                placeholder="Digite a URL da imagem do produto"
-                onChangeText={(text) => setImage(text)}
-                value={image}
-            />
-            <TouchableOpacity onPress={() => handleProducts()}>
-                <Text>Enviar</Text>
-            </TouchableOpacity>
+                <Text style={styles.productsProperty}>Imagem:</Text>
+                <TextInput
+                    style={styles.productsValue}
+                    placeholder="Digite a URL da imagem do produto"
+                    onChangeText={(text) => setImage(text)}
+                    value={image}
+                />
+                <TouchableOpacity style={styles.action} onPress={() => handleProducts()}>
+                    <Text style={styles.buttonText} >Cadastrar</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
--- mobile/src/pages/Register/styles.js
+++ mobile/src/pages/Register/styles.js
@@ -14,4 +14,54 @@ export default StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center'
     },
+
+    product: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#FFF',
+        marginBottom: 16,
+        marginTop: 20,
+    },
+
+    productsPropertyName:{
+        fontSize: 14,
+        color: '#41414d',
+        fontWeight: 'bold',
+    },
+
+    productsProperty:{
+        marginTop: -14,
+        fontSize: 14,
+        color: '#41414d',
+        fontWeight: 'bold',
+    },
+
+    productsValue: {
+        marginTop: 5,
+        fontSize: 15,
+        marginBottom: 24,
+        color: '#737380'
+    },
+
+    action: {
+        backgroundColor: '#23b3b8',
+        borderRadius: 8,
+        
+        height: 50,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    select: {
+        height: 50, 
+        width: '100%',
+        marginBottom: 10 
+    },
+
+    buttonText: {
+        color: '#ffffff',
+        fontWeight: 'bold',
+    }
+
+
 })
